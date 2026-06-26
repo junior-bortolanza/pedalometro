c108a7069688c24a12c54935dffec9df0b9ba51e
Junior Bortolanza <gbortolanzajr@gmail.com>
2026-06-26 08:21:10 -0300

    feat(weather): integrate external weather clients

--- src/main/java/com/pedalometro/weather_api/client/GeocodingClient.java
+++ src/main/java/com/pedalometro/weather_api/client/GeocodingClient.java
@@ -1,4 +1,31 @@
 package com.pedalometro.weather_api.client;
 
+import com.pedalometro.weather_api.dto.GeoCodingResponseDTO;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
 public class GeocodingClient {
+
+    private final RestClient restClient;
+
+    public GeocodingClient(RestClient.Builder build) {
+        this.restClient = build.build();
+    }
+
+    public GeoCodingResponseDTO searchFromCity(String city) {
+
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme("https")
+                        .host("geocoding-api.open-meteo.com")
+                        .path("/v1/search")
+                        .queryParam("name", city)
+                        .queryParam("count", 1)
+                        .queryParam("language", "pt")
+                        .queryParam("format", "json")
+                        .build())
+                .retrieve()
+                .body(GeoCodingResponseDTO.class);
+    }
 }
--- src/main/java/com/pedalometro/weather_api/client/OpenMeteoClient.java
+++ src/main/java/com/pedalometro/weather_api/client/OpenMeteoClient.java
@@ -1,5 +1,6 @@
 package com.pedalometro.weather_api.client;
 
+import com.pedalometro.weather_api.dto.OpenMeteoResponseDTO;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -11,7 +12,9 @@ public class OpenMeteoClient {
         this.restClient = builder.build();
     }
 
-    public String getWeather(Double latitude, Double longitude) {
+    public OpenMeteoResponseDTO getWeather(
+            Double latitude,
+            Double longitude) {
 
         return restClient.get()
                 .uri(uriBuilder -> uriBuilder
@@ -24,10 +27,11 @@ public class OpenMeteoClient {
                                 "temperature_2m,wind_speed_10m")
                         .queryParam("hourly",
                                 "precipitation_probability,wind_speed_10m")
+                        .queryParam("daily", "sunrise,sunset")
+                        .queryParam("forecast_days", 1)
                         .queryParam("timezone", "auto")
                         .build())
                 .retrieve()
-                .body(String.class);
+                .body(OpenMeteoResponseDTO.class);
     }
-
 }
