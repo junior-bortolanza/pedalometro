e993bed9f3a1c498fd9a1b326e598695fe5ba312
Junior Bortolanza <gbortolanzajr@gmail.com>
2026-06-26 08:30:32 -0300

    feat: Implementing weather controller

--- src/main/java/com/pedalometro/weather_api/controller/WeatherController.java
+++ src/main/java/com/pedalometro/weather_api/controller/WeatherController.java
@@ -1,18 +1,22 @@
 package com.pedalometro.weather_api.controller;
 
-import com.pedalometro.weather_api.client.OpenMeteoClient;
+import com.pedalometro.weather_api.dto.PedalometroResponseDTO;
+import com.pedalometro.weather_api.service.WeatherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/teste")
+@RequestMapping("/weather")
 @RequiredArgsConstructor
-public class TesteController {
-    private final OpenMeteoClient openMeteoClient;
+public class WeatherController {
 
+    private final WeatherService weatherService;
 
-
-
+    @GetMapping
+    public PedalometroResponseDTO getWeather(@RequestParam String city) {
+        return weatherService.getWeather(city);
+    }
 }
